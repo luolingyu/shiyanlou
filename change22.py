6ec7f6b690078cf6216583ca3d7fbca0999f7992
luolingyu <1324013683@qq.com>
2018-06-22 21:04:32 +0800

    Pandas 处理Json文件

---
+++ change22.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import sys
+
+
+def analysis(file,id):
+    df =  pd.read_json(file)
+    d = df[df['user_id']== id]
+    times = len(d)
+    minutes = d['minutes'].sum()
+    return times,minutes
+
+
+if __name__=='__main__':
+    args = sys.argv[1:]
+    '''
+    for arg in args:
+        file,sid = arg.split(':')
+        if not file or id:
+            print(0)
+    id = int(sid) 
+    '''
+    file, id = args
+    print(analysis(file, int(id)))
